5a1499fc14ef0235fbd7b9ebc0cde9182dcc8d00
nmauersberg <n.mauersberg@gmail.com>
2022-09-02 17:55:57 +0200

    Init organicer app

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+});
+
+const nextConfig = withPWA({
   reactStrictMode: true,
   swcMinify: true,
-}
+});
 
-module.exports = nextConfig
+module.exports = nextConfig;
